19ad0cd1905f8f660944c315222c153737eba61e
Arthur Donou <donou.arthur@gmail.com>
2023-05-02 16:24:40 +0200

    ajout de la fonction de tri de cv et de l'affichage de cv garder

--- classcv.py
+++ classcv.py
@@ -1,4 +1,15 @@
-def tri_cv_parbac(liste,niveau):
+def tri_cv_parbac(liste,niveau,type):
+    cvagarder=[]
     for i in range(len(liste)):
-        
+        if liste[i][1]>=niveau and liste[i][2]==type:
+            cvagarder.append(i)
+    return cvagarder
 
+def affichage_bon_cv(liste_cv,index):
+    for i in range(len(index)):
+        print(liste_cv[index[i]])
+
+liste=[["Cringe Lord",4,"buveur de bite"],["Arnaud Donou",5,"Informatique"],["Guillaume Puill",2,"Alcoolique annonyme"],["Arthur Donou",8,"Informatique"],["Mr le docteur",9,"Medecine"],["Un pelo random",3,"Informatique"]]
+
+print(tri_cv_parbac(liste,3,"Informatique"))
+affichage_bon_cv(liste,tri_cv_parbac(liste,3,"Informatique"))
