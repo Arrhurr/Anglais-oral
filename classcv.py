11bb9a117907e268152422fc51e99665023edb85
Arthur Donou <donou.arthur@gmail.com>
2023-05-12 09:48:23 +0200

    ajout d'un recherche du minimum

--- classcv.py
+++ classcv.py
@@ -1,13 +1,20 @@
-def tri_cv_parbac(liste:list,niveau:int,type=None,nbvoulu=None):
+def tri_cv_parbac(liste:list,niveau:int,etude=None,nbvoulu=None):
     cvagarder=[]
-    if nbvoulu.type()!=int:
+    if type(nbvoulu)!=int:
         max=len(liste)+1
     else:max=nbvoulu
-    if type is not None: type=type.lower()
+    if etude is not None: etude=etude.lower()
     for i in range(len(liste)):
-        if liste[i][1]>=niveau and liste[i][2].lower()==type and max>0: 
+        if liste[i][1]>=niveau and liste[i][2].lower()==etude: 
             cvagarder.append(i)
-            max-=1
+    while len(cvagarder)>max:
+        min=liste[cvagarder[0]][1]
+        mini=0
+        for i in range(len(cvagarder)):
+            if liste[cvagarder[i]][1]<min:
+                min=liste[cvagarder[i]][1]
+                mini=i
+        cvagarder.pop(mini)
     return cvagarder
 
 def affichage_bon_cv(liste_cv:list,index:list):
@@ -17,5 +24,4 @@ def affichage_bon_cv(liste_cv:list,index:list):
 
 liste=[["Cringe Lord",4,"buveur de bite"],["Arnaud Donou",5,"Informatique"],["Guillaume Puill",2,"Alcoolique annonyme"],["Arthur Donou",8,"Informatique"],["Mr le docteur",9,"Medecine"],["Un pelo random",3,"Informatique"]]
 
-print(tri_cv_parbac(liste,3,nbvoulu=2))
-affichage_bon_cv(liste,tri_cv_parbac(liste,3,type="Informatique",nbvoulu=2))
+affichage_bon_cv(liste,tri_cv_parbac(liste,3,etude="Informatique",nbvoulu=2))
